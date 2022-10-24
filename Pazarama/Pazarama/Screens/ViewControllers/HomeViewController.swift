12fbe14d60976d1935c89b31a3ed26ad058f1427
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2022-10-25 01:32:34 +0300

    lottie animation homeview

--- Pazarama/Pazarama/Screens/ViewControllers/HomeViewController.swift
+++ Pazarama/Pazarama/Screens/ViewControllers/HomeViewController.swift
@@ -6,13 +6,22 @@
 //
 
 import UIKit
-
+import Lottie
 class HomeViewController: UIViewController {
 
+    var animationView = LottieAnimationView()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = .systemIndigo
+        // load init under assets folder
+        animationView = LottieAnimationView(name: "ecommerce")
+        animationView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        animationView.center = view.center
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
     }
 
 
