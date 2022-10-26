e3da58a26692e508db5932c8a33a23fe85aca6d3
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2022-10-27 00:55:34 +0300

    category page update

--- Pazarama/Pazarama/Screens/ViewControllers/CategoryViewController.swift
+++ Pazarama/Pazarama/Screens/ViewControllers/CategoryViewController.swift
@@ -7,9 +7,19 @@
 
 import UIKit
 
-class CategoryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ResultVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
+class CategoryViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UISearchResultsUpdating, UISearchBarDelegate {
 
     // MARK: - Properties
+
+    let searchController = UISearchController()
+    var filteredArray = [CategoryModal]()
 
     private let categoryList: [CategoryModal] = [
         CategoryModal(name: "Electronics", image: "electronics"),
@@ -19,10 +29,12 @@ class CategoryViewController: UIViewController, UICollectionViewDataSource, UICo
         
     ]
 
-       private let collectionView: UICollectionView = {
+    // MARK: - UI Elements
+
+    private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.itemSize.width = UIScreen.main.bounds.width / 2.5
-        layout.itemSize.height = UIScreen.main.bounds.height / 2.5 - 30
+        layout.itemSize.height = UIScreen.main.bounds.width / 2.5
         layout.scrollDirection = .vertical
         
         layout.sectionInset = UIEdgeInsets(top: 30, left: 30, bottom: 0, right: 30)
@@ -37,7 +49,10 @@ class CategoryViewController: UIViewController, UICollectionViewDataSource, UICo
         super.viewDidLoad()
         configureUI()
         view.backgroundColor = .white
-        
+        searchController.searchBar.delegate = self
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+
     }
 
     // MARK: - Helpers
@@ -45,36 +60,55 @@ class CategoryViewController: UIViewController, UICollectionViewDataSource, UICo
     func configureUI() {
         //view.backgroundColor = .white
         navigationItem.title = "Categories"
-       
-        
+
+        // collection
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "Category")
         view.addSubview(collectionView)
-        collectionView.frame = view.frame 
+        collectionView.frame = view.frame
+
+        // search controller
+        searchController.searchBar.placeholder = "Search"
+
         
     }
 
 
     // MARK: - Overrides
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return categoryList.count ?? .zero
+            if searchController.isActive && searchController.searchBar.text != "" {
+                return filteredArray.count ?? .zero
+            }
+            else {
+                return categoryList.count ?? .zero
+            }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Category", for: indexPath) as! CategoryCollectionViewCell
-        
-        cell.categoryTitle.text = categoryList[indexPath.row].name
-        cell.categoryImage.image = UIImage(named: categoryList[indexPath.row].image)
+        if searchController.isActive && searchController.searchBar.text != "" {
+            cell.categoryImage.image = UIImage(named: filteredArray[indexPath.row].image)
+        } else {
+            cell.categoryImage.image = UIImage(named: categoryList[indexPath.row].image)
+        }
         
         return cell
     }
 
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(categoryList[indexPath.row].name)        
+    
+
+
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        let filtered = categoryList.filter { $0.name.lowercased().contains(text.lowercased()) }
+        filteredArray = filtered
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+        print(filtered)
     }
 
-    
 
 
 
