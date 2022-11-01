5bedd9ec49aad33f70b018704fd3b3fa7cc10383
utkusapaz <utku.sapaz@std.yeditepe.edu.tr>
2022-11-02 01:55:03 +0300

    login, shopping cart, register updates

---
+++ Pazarama/Pazarama/Screens/ShoppingCart/ViewModal/ShoppingCartViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  ShoppingCartViewModel.swift
+//  Pazarama
+//
+//  Created by utku on 2.11.2022.
+//
+
+import Foundation
+
+class ShoppingCartViewModel{
+    // MARK: - Properties
+    var shoppingCart: [ProductItem] = [] {
+        didSet {
+            self.shoppingCartChanged?()
+        }
+    }
+
+    var shoppingCartChanged: (() -> Void)?
+    
+    
+    // MARK: - Methods
+    
+    // get all products from core data
+    func getAllProducts() {
+        shoppingCart = Product.getAllProducts()
+        print("Shopping Cart: \(shoppingCart)")
+        
+    }
+    
+    
+    func calculateTotalPrice() -> Double {
+        var totalPrice = 0
+        for item in shoppingCart {
+            totalPrice += Int(item.price * Double(item.quantity))
+        }
+        return Double(totalPrice)
+    }
+}
